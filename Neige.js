e4ab9167cddf40c9c339500f667f2357c0384601
Lefido <lefido@hotmail.com>
2022-12-21 14:40:33 +0100

    Seven  commit

--- Neige.js
+++ Neige.js
@@ -1,55 +1,45 @@
-
-
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 
 export default class Neige {
+  constructor(x, y, w, h, vx, vy) {
+    let flocon = document.createElement("div");
+    flocon.classList.add("flocon");
+    flocon.style.backgroundColor = "gray";
+    flocon.style.position = "absolute";
+    flocon.style.borderRadius = "50%";
+    flocon.style.opacity = "0.2";
 
-    constructor(x, y, w, h, vx, vy) {
+    body.appendChild(flocon);
 
-        let flocon = document.createElement('div')
-        flocon.classList.add("flocon");
-        flocon.style.backgroundColor = "gray";
-        flocon.style.position = "absolute"
-        flocon.style.borderRadius = "50%"
-        flocon.style.opacity = "0.2"
-        
-        body.appendChild(flocon);
+    this.flocon = flocon;
+    this.flocon.style.width = w + "px";
+    this.flocon.style.height = h + "px";
+    this.flocon.style.left = x + "px";
+    this.flocon.style.top = y + "px";
+    this.y = y;
+    this.x = x;
+    this.vy = vy;
+    this.vx = vx;
+  }
 
-        this.flocon = flocon;
-        this.flocon.style.width = w + "px"
-        this.flocon.style.height = h + "px"
-        this.flocon.style.left = x + "px"
-        this.flocon.style.top = y + "px"
-        this.y = y
-        this.x= x
-        this.vy = vy
-        this.vx = vx
-    }
+  move() {
+    this.y += this.vy;
+    this.x = this.x + this.vx;
+    this.flocon.style.top = this.y + "px";
+    this.flocon.style.left = this.x + "px";
 
-    move() {
+    // let direction = Math.floor(Math.random() * 3);
 
-        this.y += this.vy
-        this.x = this.x  + this.vx
-        this.flocon.style.top = this.y + "px"
-        this.flocon.style.left = this.x + "px"
+    // if (direction === 1 ) {
+    //     this.vx = 5
+    //     console.log("direction droite")
+    // } else {
+    //     this.vx = -5
+    //     console.log("direction gauche")
+    // }
+  }
 
-        // let direction = Math.floor(Math.random() * 3);
-
-        // if (direction === 1 ) {
-        //     this.vx = 5
-        //     console.log("direction droite")
-        // } else {
-        //     this.vx = -5
-        //     console.log("direction gauche")
-        // }
-
-    }
-
-    remove() {
-
-        this.flocon.remove();
-
-    }
-
-
+  remove() {
+    this.flocon.remove();
+  }
 }
--- script.js
+++ script.js
@@ -1,76 +1,61 @@
-
 import Neige from "./Neige.js";
 
-const btn_up = document.querySelector('.btn-up');
-const body = document.querySelector('body')
+const btn_up = document.querySelector(".btn-up");
+const body = document.querySelector("body");
 
-var navMobile = mobilecheck()
+var navMobile = mobilecheck();
 
-window.addEventListener('scroll', () => {
-    if(window.scrollY > 80) {
-        btn_up.classList.add('btn-scroll');
-    } else {
-        btn_up.classList.remove('btn-scroll');
-    }
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    btn_up.classList.add("btn-scroll");
+  } else {
+    btn_up.classList.remove("btn-scroll");
+  }
 });
-
 
 var flocons = [];
 
 if (!navMobile) {
+  for (let i = 0; i < 150; i++) {
+    new_flocon();
+  }
 
-    for (let i = 0; i < 150; i++) {
-
-        new_flocon()
-       
-    }
-
-    setInterval(playing, 1000/60)
-
+  setInterval(playing, 1000 / 60);
 }
 
 function playing() {
+  flocons.forEach((flocon, id) => {
+    flocon.move();
 
-
-flocons.forEach((flocon, id) => {
-
-    flocon.move()
-
-    if (flocon.y > body.offsetHeight -20) {
-        flocons.splice(id,1);
-        flocon.remove()
-        new_flocon()
+    if (flocon.y > body.offsetHeight - 20) {
+      flocons.splice(id, 1);
+      flocon.remove();
+      new_flocon();
     }
-
-})
-
-};
-
+  });
+}
 
 function new_flocon() {
+  let taille = rnd(10);
+  let flocon = new Neige(
+    rnd(innerWidth - 20),
+    -rnd(innerHeight),
+    taille,
+    taille,
+    0,
+    rnd(10)
+  );
 
-    let taille = rnd(10)
-    let flocon = new Neige(rnd(innerWidth-20), -rnd(innerHeight), taille, taille,0, rnd(10))
-
-    flocons.push(flocon);
-
+  flocons.push(flocon);
 }
 
 function rnd(valeur) {
-
-    return 3 + Math.floor(Math.random() * valeur);
-
+  return 3 + Math.floor(Math.random() * valeur);
 }
 
-
 function mobilecheck() {
-    return (typeof window.orientation !== "undefined") 
-      || (navigator.userAgent.indexOf('IEMobile') !== -1
-      );
-};
-
-
-
-
-
-
+  return (
+    typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf("IEMobile") !== -1
+  );
+}
